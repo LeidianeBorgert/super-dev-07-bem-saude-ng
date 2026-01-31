857cffc537f773ec2b149f6d8d095d9b4ac0dffc
Leidiane Borgert <leidianeborgert@gmail.com>
2026-01-30 21:57:35 -0300

    atualizado paginas pacientes e CRUD

--- src/app/models/recepcionista.model.ts
+++ src/app/models/recepcionista.model.ts
@@ -1,6 +1,20 @@
 export interface RecepcionistaResponseModel{
 
-    id:number;
+    id:string;
     nome:string;
     status: boolean;
+}
+
+export interface RecepcionistaPesquisaResponseModel{
+    id:string;
+    nome:string;
+    status: boolean;
+}
+
+export interface RecepcionistaCriarRequestModel{
+    nome:string;
+}
+
+export interface RecepcionistaEditarRequestModel{
+    nome:string;
 }
---
+++ src/app/services/recepcionista.service.ts
@@ -0,0 +1,39 @@
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RecepcionistaCriarRequestModel, RecepcionistaEditarRequestModel, RecepcionistaPesquisaResponseModel, RecepcionistaResponseModel } from '../models/recepcionista.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecepcionistaService {
+  private httpClient = inject(HttpClient);
+
+  getAll(): Observable<RecepcionistaResponseModel[]>{
+    const url = `${environment.apiUrl}/recepcionistas`;
+    return this.httpClient.get<RecepcionistaResponseModel[]>(url);
+    
+  }
+  
+  create(form:RecepcionistaCriarRequestModel):Observable<RecepcionistaResponseModel>{
+    const url = `${environment.apiUrl}/recepcionistas`;
+    return this.httpClient.post<RecepcionistaResponseModel>(url, form);
+  }
+
+  update(id: string, form: RecepcionistaEditarRequestModel):Observable<RecepcionistaResponseModel>{
+    const url = `${environment.apiUrl}/recepcionistas/${id}`;
+    return this.httpClient.put<RecepcionistaResponseModel>(url, form);
+  }
+
+  getById(id:string): Observable<RecepcionistaPesquisaResponseModel>{
+    const url = `${environment.apiUrl}/recepcionistas/${id}`;
+    return this.httpClient.get<RecepcionistaPesquisaResponseModel>(url);
+  }
+
+  delete(id:string):Observable<void>{
+    const url = `${environment.apiUrl}/recepcionistas/${id}`;
+    return this.httpClient.delete<void>(url);
+  }
+}
